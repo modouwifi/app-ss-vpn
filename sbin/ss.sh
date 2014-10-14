703727d5e1717f74c80973ab62a0cad4eca88e4f
JunJian <junjian@mochui.net>
2014-10-14 20:50:26 +0800

    add the pdnsd to avoid the dns error

--- sbin/ss.sh
+++ sbin/ss.sh
@@ -3,9 +3,13 @@
 CURWDIR=$(cd $(dirname $0) && pwd) CUSTOMCONF="$CURWDIR/../conf/custom.conf"
 SETCONFDEF="$CURWDIR/../conf/set.def"
 SETCONF="$CURWDIR/../conf/set.conf"
+PDNSDCONFILE="$CURWDIR/../conf/pdnsd.conf"
 PIDFILE="$CURWDIR/../conf/custom.pid"
 SSBIN="$CURWDIR/../bin/ss-redir"
 SSSHELL="$CURWDIR/../sbin/ss-transp.sh "
+PDNSDBIN="$CURWDIR/../bin/pdnsd"
+DNSMASQCONF="/system/conf/dnsmasq.conf"
+PDNSDCONF="conf-dir=$CURWDIR/../conf/dnsmasq";
 
 CMDHEAD='"cmd":"'
 CMDTAIL='",'
@@ -15,6 +19,48 @@ SHELLBUTTON22="$CURWDIR/../sbin/ss.sh stop"
 CMDBUTTON1=${CMDHEAD}${SHELLBUTTON1}${CMDTAIL};
 CMDBUTTON2=${CMDHEAD}${SHELLBUTTON2}${CMDTAIL};
 CMDBUTTON22=${CMDHEAD}${SHELLBUTTON22}${CMDTAIL};
+
+## add the change of dnsmasq conf for pdnsd
+## fork from HDNS
+delConfDir() {
+    /system/sbin/writesys.sh
+    local pdnsddir=`echo "$CURWDIR/../conf/dnsmasq" | sed -e 's:/:\\\\/:g'`
+    /bin/sed -ie "/conf-dir=$pdnsddir/d" $DNSMASQCONF 1>/dev/null 2>&1
+    /system/sbin/writesys.sh close
+}
+
+addConfDir() {
+    /system/sbin/writesys.sh
+    local pdnsddir=`echo "$CURWDIR/../conf/dnsmasq" | sed -e 's:/:\\\\/:g'`
+    /bin/sed -ie "/conf-dir=$pdnsddir/d" $DNSMASQCONF 1>/dev/null 2>&1
+    echo $PDNSDCONF >> $DNSMASQCONF
+    /system/sbin/writesys.sh close
+}
+
+dnsStop() {
+    pid=`/bin/ps|grep dnsmasq|grep -v grep|awk '{print $1}'`
+    if [ "$pid" != "" ]; then
+        /bin/kill $pid 1>/dev/null 2>&1
+    else
+        /usr/bin/killall dnsmasq 1>/dev/null 2>&1
+    fi
+}
+
+dnsStart() {
+    dnsStop
+    /bin/dnsmasq -C "$DNSMASQCONF" &
+}
+
+pdnsdEnable() {
+    addConfDir
+    dnsStart
+}
+
+pdnsdDisable() {
+    delConfDir
+    killall pdnsd 1>/dev/null 2>&1
+    dnsStart
+}
 
 testServerStatus()
 {
@@ -131,11 +177,14 @@ ssConfig()
 
 ssStart()
 {
+    pdnsdEnable;
     serveraddr=`head -n 1 $SETCONF | cut -d ' ' -f2-`;
     serverport=`head -n 2 $SETCONF | tail -n 1 | cut -d ' ' -f2-`;
     secmode=`head -n 3 $SETCONF | tail -n 1 | cut -d ' ' -f2-`;
     passwd=`head -n 4 $SETCONF | tail -n 1 | cut -d ' ' -f2-`;
     $SSSHELL $serveraddr $serverport $secmode $passwd &
+    chown matrix $PDNSDCONFILE 1>/dev/null 2>&1
+    $PDNSDBIN -c $PDNSDCONFILE &
     sleep 2
     genCustomConfig;
     pid=`cat $PIDFILE 2>/dev/null`;
@@ -145,6 +194,7 @@ ssStart()
 
 ssStop()
 {
+    pdnsdDisable;
     killall ss-redir 1>/dev/null 2>&1;
     iptables -t nat -F SHADOWSOCKS 1>/dev/null 2>&1
     iptables -t nat -F OUTPUT 1>/dev/null 2>&1
